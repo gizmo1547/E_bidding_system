d7322b722e24e32fa46685f52644f6a5bfaf7cd9
gizmo1547 <120990499+gizmo1547@users.noreply.github.com>
2024-10-05 16:05:45 -0400

    Tested backend post method and install react app for frontend

--- backend/index.js
+++ backend/index.js
@@ -14,12 +14,15 @@ const db = mysql.createConnection({
 //IF there is a auth problem
 //ALTER USER 'root'@localhost' IDENTIFIED WITH mysql_native_password BY 'GermanVoronovich';
 
+
+app.use(express.json())
+
 //Test get method
 app.get("/", (req,res)=>{
     res.json("hello this is the backend from here to there:)")
 })
 
-//User table from mySQL
+//Test User table from mySQL
 app.get("/user", (req,res)=>{
   const q = "SELECT * FROM user"
 db.query(q,(err,data)=>{
@@ -27,6 +30,7 @@ db.query(q,(err,data)=>{
         return res.json(data)
   })
 })
+
 /*
 //Testing post method
 app.post("/user", (req, res) => {
@@ -53,7 +57,35 @@ app.post("/user", (req, res) => {
   });
   
 */
-//Port number 8000
+
+//Testing post method
+app.post("/user", (req, res) => {
+  const q = "INSERT INTO user (Username, Password, Email, Role, AccountBalance, IsVIP, IsSuspended, SuspensionCount, AverageRating, NumberOfTransactions, IsActive, RegistrationDate) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW())";
+  
+  const values = [
+    req.body.username,          // "Bruce"
+    req.body.password,          // "23er"
+    req.body.email,             // "Bruce@example.com"
+    req.body.role || 'User',    // Use default 'User' if Role is missing
+    req.body.accountBalance,    // 1.5
+    req.body.isVIP || 0,        // Use default 0 if isVIP is missing
+    req.body.isSuspended || 0,  // Use default 0 if isSuspended is missing
+    req.body.suspensionCount || 0, // 0
+    req.body.averageRating || 0,   // 4.5
+    req.body.numberOfRatings || 0, // 0
+    req.body.numberOfTransactions || 0, // 10
+    req.body.isActive || 1  
+  
+  ];
+
+  db.query(q, values, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+//Connecting to backend, port number 8000
 app.listen(8000, ()=>{
     console.log("Connected to backend!")
 })
+    
